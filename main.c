97a9637102bb4014ead7d515cace2c261a48abe6
helene <helene.maja@hotmail.dk>
2026-01-31 00:28:16 +0100

    add another moveset to rotary encoder

--- main.c
+++ main.c
@@ -41,33 +41,22 @@ void __interrupt() isr() // interrupt vector
 {
     if (IOCIF && (IOCAF)) // if IOC set and IOC flag
     {
-        // if (PORTAbits.RA0 != PORTAbits.RA1) // B != A
-        // {
-        //     // knob is turned clockwise
-        //     cursor_pos++;
-        //     cursor_pos = cursor_pos % 16; // 16  columns
-        //     cursor_update_flag = 1;
-            
-        // }
-        // else if (PORTAbits.RA0 == PORTAbits.RA1)
-        // {
-        //     // knob is turned counterclockwise
-        //     cursor_pos--;
-        //     cursor_pos = cursor_pos % 16; // 16  columns
-        //     cursor_update_flag = 1;
-        // }
-        // else if (!PORTAbits.RA0)
-        // {
-        //     // knob is turned counterclockwise
-        //     cursor_pos++;
-        //     cursor_pos = cursor_pos % 16; // 16  columns
-        //     cursor_update_flag = 1;
-        // }
+
+        // clear flag
+        IOCAF = 0;
+        IOCIF = 0;
+
         a = PORTAbits.RA0;
         b = PORTAbits.RA1;
         
         current_rotary_state = (uint8_t)((a << 1) | b);
-        // turn clockwise
+        // turn clockwise 
+        // pattern
+        // 11    01
+        // 01    00
+        // 00    10
+        // 10    11
+        // prev, curr
         if (previous_rotary_state == ((1 << 1) | 1) && current_rotary_state == ((0 << 1) | 1))
         {
             acc++;
@@ -85,21 +74,44 @@ void __interrupt() isr() // interrupt vector
             acc++;
         }
 
+        if (previous_rotary_state == ((1 << 1) | 1) && current_rotary_state == ((1 << 1) | 0))
+        {
+            acc--;
+        }
+        else if (previous_rotary_state == ((1 << 1) | 0) && current_rotary_state == ((0 << 1) | 0))
+        {
+            acc--;
+        }
+        else if (previous_rotary_state == ((0 << 1) | 0) && current_rotary_state == ((0 << 1) | 1))
+        {
+            acc--;
+        }
+        else if (previous_rotary_state == ((0 << 1) | 1) && current_rotary_state == ((1 << 1) | 1))
+        {
+            acc--;
+        }
+        
+
+
         if (acc >= 4 && current_rotary_state == ((1 << 1) | 1))
         {
-            cursor_pos++;
-            if (cursor_pos > 15)
-            {
-                cursor_pos = 0;
-            }
+            cursor_pos = (cursor_pos + 1) & 0x0F;
+            cursor_update_flag = 1;
+            acc = 0;
+        }
+        else if (acc <= -4 && current_rotary_state == ((1 << 1) | 1))
+        {
+            cursor_pos = (cursor_pos -1) & 0x0F;
+            // if (cursor_pos > 15)
+            // {
+            //     cursor_pos = 0;
+            // }
             cursor_update_flag = 1;
             acc = 0;
         }
         previous_rotary_state = current_rotary_state;
         
-        // clear flag
-        IOCAF = 0;
-        IOCIF = 0;
+        
         
     }
 }
@@ -114,48 +126,40 @@ int main()
     WPUA = 0b00000000;    //  weak pull ups
     TMR0 = 0;
     OSCCON = 0b11111111; // internal oscillator to max frequency
-    // OSCCON = 0b01101010;
     OPTION_REG = 0b00000111;
-    // INTCON = 0b11110000;
     INTCON = 0b10001000; // interrupt on change enable bit set
 
-    IOCAP =  0b00000011;
+    IOCAP =  0b00000011; // rising edge
     IOCAF = 0; // flag
-    IOCANbits.IOCAN0 = 1;        
-    IOCANbits.IOCAN1 = 1;
-    // try setting sda to output
-    // TRISAbits.TRISA4 = 0;
-    // uint8_t test_byte = 0b00101110;
-    // LCD1602_init(16,2);
+    IOCANbits.IOCAN0 = 1; // falling edge        
+    IOCANbits.IOCAN1 = 1; // falling edge 
+
     LCD1602_init(16, 2);
 
-    
-
-    // LCD1602_setCursor(5, 1);        // move cursor somewhere visible
-    // LCD1602_cursor_blink_on();      // enable blinking cursor
-
-    // __delay_ms(10);
-    // LCD_Init(0x7E); // Initialize LCD module with I2C address = 0x4E
-    
-    // LCD_Set_Cursor(1, 1);
-    // LCD_Write_String(" Khaled Magdy");
-    // LCD_Set_Cursor(2, 1);
-    // LCD_Write_String(" DeepBlue");
     LCD1602_setCursor(0, 0);
-    LCD1602_print("hello");
+    LCD1602_print("[ ]");
+    LCD1602_setCursor(0, 1);
+    LCD1602_print("[  ]");
+
+    LCD1602_setCursor(7, 0);
+    LCD1602_print("Vand (L)");
+    LCD1602_setCursor(7, 1);
+    LCD1602_print("Temp (");
+    LCD1602_setCursor(13, 1);
+    char dot = 0xdf;
+    LCD1602_print(&dot);
+    LCD1602_setCursor(14, 1);
+    LCD1602_print("C)");
     LCD1602_setCursor(cursor_pos, 0);
     LCD1602_cursor_blink_on();
     while (1)
     {
-        // i2c_start();
-        // i2c_stop();
-        // i2c_write_byte(test_byte);
-        // setCursor(0,0);
-        // send_string("hello");
+
         if (cursor_update_flag)
         {
             LCD1602_setCursor(cursor_pos, 0);
             cursor_update_flag = 0;
+            __delay_ms(1);
         }
         
 
