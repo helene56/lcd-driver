4983fae8e66a0796e5ce5f10e9014a2d8a651885
helene <helene.maja@hotmail.dk>
2026-02-01 19:26:05 +0100

    add qty to menu

--- main.c
+++ main.c
@@ -25,7 +25,7 @@
 #pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
 // No CPD config for PIC12F1571
 volatile uint8_t counter = 0;
-volatile uint8_t cursor_pos = 9;
+volatile uint8_t cursor_pos = 1;
 volatile uint8_t cursor_update_flag = 0;
 volatile uint8_t confirm_update_flag = 0;
 volatile uint8_t a = 1;
@@ -37,6 +37,19 @@ volatile uint8_t previous_rotary_state = (1 << 1) | 1;
 static int8_t states[] = {0, -1, 1, 0, 1, 0,
                           0, -1, -1, 0, 0,
                            1, 0, 1, -1, 0};
+
+
+
+volatile uint8_t water_confirm = 0;
+volatile uint8_t temp_confirm = 0;
+volatile uint8_t row_confirm = 0;
+volatile uint8_t select_row = 0;
+volatile uint8_t water_qty = 2;
+volatile uint8_t temp_qty = 65;
+char wat_display[]  = "002";
+char temp_display[] = "065";
+char empty_val[] = " ";
+char confirm_val[] = "x";
 
 void __interrupt() isr() // interrupt vector
 {
@@ -64,13 +77,52 @@ void __interrupt() isr() // interrupt vector
 
         if (acc >= 4 && current_rotary_state == ((1 << 1) | 1))
         {
-            cursor_pos = (cursor_pos + 1) & 0x0F;
+            // cursor_pos = (cursor_pos + 1) & 0x0F;
+            if (row_confirm == 1)
+            {
+                // then we move the quanity of current row
+                if (select_row == 0)
+                {
+                    // water_qty = (water_qty + 1) & 0x14;
+                    water_qty++;
+                }
+                else if (select_row == 1)
+                {
+                    // temp_qty = (temp_qty + 5) & 0x64;
+                    temp_qty += 5;
+                }
+            }
+            else
+            {
+                // toggle the row value only want row 0 or row 1
+                select_row = 1;
+            }
             cursor_update_flag = 1;
             acc = 0;
         }
         else if (acc <= -4 && current_rotary_state == ((1 << 1) | 1))
         {
-            cursor_pos = (cursor_pos -1) & 0x0F;
+            // cursor_pos = (cursor_pos -1) & 0x0F;
+            if (row_confirm == 1)
+            {
+                // then we move the quanity of current row
+                if (select_row == 0)
+                {
+                    // water_qty = (water_qty - 1) & 0x14;
+                    water_qty--;
+                }
+                else if (select_row == 1)
+                {
+                    // temp_qty = (temp_qty - 5) & 0x64;
+                    temp_qty -= 5;
+                }
+            }
+            else
+            {
+                // toggle the row value only want row 0 or row 1
+                select_row = 0;
+            }
+
             cursor_update_flag = 1;
             acc = 0;
         }
@@ -116,12 +168,12 @@ int main()
     LCD1602_print("[ ]");
     // start quantity water
     LCD1602_setCursor(4, 0);
-    LCD1602_print("100");
+    LCD1602_print(wat_display);
     // start quantity temperature
     LCD1602_setCursor(4, 1);
-    LCD1602_print("65");
+    LCD1602_print(temp_display);
     LCD1602_setCursor(8, 0);
-    LCD1602_print("Vand(mL)");
+    LCD1602_print("Vand(dL)");
     LCD1602_setCursor(8, 1);
     LCD1602_print("Temp(");
     LCD1602_setCursor(13, 1);
@@ -130,21 +182,107 @@ int main()
     LCD1602_setCursor(14, 1);
     LCD1602_print("C)");
     LCD1602_setCursor(cursor_pos, 0);
-    // LCD1602_cursor_blink_on();
-    LCD1602_setCursor(1, 0);
+    LCD1602_cursor_blink_on();
+
     while (1)
     {
 
         if (cursor_update_flag)
         {
-            LCD1602_setCursor(cursor_pos, 0);
+            if (row_confirm == 1)
+            {
+                // then we update the water/temp quantity display
+                if (select_row == 0)
+                {
+                    // update water qty
+                    wat_display[0] = '0' + ((water_qty / 100) % 10);
+                    wat_display[1] = '0' + ((water_qty / 10) % 10);
+                    wat_display[2] = '0' + (water_qty % 10);
+                    LCD1602_setCursor(4, select_row);
+                    LCD1602_print(wat_display);
+                    
+                }
+                else if (select_row == 1)
+                {
+                    // update water qty
+                    temp_display[0] = '0' + ((temp_qty / 100) % 10);
+                    temp_display[1] = '0' + ((temp_qty / 10) % 10);
+                    temp_display[2] = '0' + (temp_qty % 10);
+                    LCD1602_setCursor(4, select_row);
+                    LCD1602_print(temp_display);
+                    
+                }
+                LCD1602_setCursor(cursor_pos, select_row);
+
+
+            }
+            else
+            {
+                LCD1602_setCursor(cursor_pos, select_row);
+                
+            }
             cursor_update_flag = 0;
             __delay_ms(1);
+
+            // LCD1602_setCursor(cursor_pos, 0);
+            // cursor_update_flag = 0;
+            // __delay_ms(1);
         }
         if (confirm_update_flag)
         {
-            LCD1602_setCursor(cursor_pos, 0);
-            LCD1602_print("x");
+            // LCD1602_setCursor(cursor_pos, 0);
+            LCD1602_setCursor(cursor_pos, select_row);
+            if (row_confirm == 0)
+            {
+                // row has been selected - now qty can be chosen
+                row_confirm = 1;
+                // next time it should select and put and x
+
+            }
+            else if (row_confirm == 1)
+            {
+                // qty has been confirmed and it will be marked with x
+                // now we can move between the two quanitites again
+                if (select_row == 0)
+                {
+                    water_confirm ^= 1;
+                    if (water_confirm)
+                    {
+                        LCD1602_print(confirm_val);
+                    }
+                    else
+                    {
+                        LCD1602_print(empty_val);
+                    }
+                }
+                else if (select_row == 1)
+                {
+                    temp_confirm ^= 1;
+                    if (temp_confirm)
+                    {
+                        LCD1602_print(confirm_val);
+                    }
+                    else
+                    {
+                        LCD1602_print(empty_val);
+                    }
+                }
+                LCD1602_setCursor(cursor_pos, select_row);
+                
+                // LCD1602_print("x");
+                // if (select_row == 0)
+                // {
+                //     water_confirm = 1;
+                // }
+                // else if (select_row == 1)
+                // {
+                //     temp_confirm = 1;
+                // }
+                row_confirm = 0;
+            }
+
+            
+            
             confirm_update_flag = 0;
             __delay_ms(1);
 
